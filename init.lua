37a16a04675b58a896c15c74c1e2bc2f1f22e9a5
0x00 <45222020+0x00nier@users.noreply.github.com>
2023-03-06 03:04:54 -0500

    Add files via upload

--- init.lua
+++ init.lua
@@ -163,7 +163,9 @@ require('lazy').setup({
       return vim.fn.executable 'make' == 1
     end,
   },
-
+  {
+    'nvim-telescope/telescope-file-browser.nvim' 
+  },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     dependencies = {
@@ -505,6 +507,5 @@ require('lspconfig').pylsp.setup {
     }
   }
 }
-
--- The line beneath this is called `modeline`. See `:help modeline`
+--- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
